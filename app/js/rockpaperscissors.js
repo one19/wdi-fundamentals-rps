42870b1debbfefc313d6695bb8cadf2474bdc9cd
Drew Showalter <maelstroswim@gmail.com>
2015-04-25 19:29:24 +1000

    wrote the playTo and playToFive functions on our rock-paper-scissors program

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -35,6 +35,7 @@ function getComputerMove(move) {
     return move = move || randomPlay();
 }
 
+
 function getWinner(playerMove,computerMove) {
     var winner;
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
@@ -43,9 +44,9 @@ function getWinner(playerMove,computerMove) {
     if (playerMove == computerMove) {
     	winner = 'tie';
     } else if (playerMove == 'rock' && computerMove == 'paper') {
-		winner = 'player';    
-    } else if (playerMove == 'rock' && computerMove == 'scissors') {
 		winner = 'computer';    
+    } else if (playerMove == 'rock' && computerMove == 'scissors') {
+		winner = 'player';    
     } else if (playerMove == 'paper' && computerMove == 'rock') {
 		winner = 'player';    
     } else if (playerMove == 'paper' && computerMove == 'scissors') {
@@ -63,8 +64,47 @@ function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    return [playerWins, computerWins];
+    var playerMove = null;
+    var computerMove = null;
+    while (playerWins < 5 && computerWins < 5) {
+        playerMove=getPlayerMove();
+        computerMove=getComputerMove();
+        if (getWinner(playerMove, computerMove) == 'player') {
+    	   playerWins +=1
+        } else if (getWinner(playerMove, computerMove) == 'computer') {
+    	   computerWins +=1
+        }
+        console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+        console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+        if (playerWins == 5) {
+            return 'Player Wins!'
+        } else if (computerWins == 5) {
+            return 'Computer Wins!'
+        }
+    }
 }
 
+//play to x function, input the number of wins necissary to end loop
+function playTo(x) {
+    console.log("Let's play Rock, Paper, Scissors");
+    var playerWins = 0;
+    var computerWins = 0;
+    var playerMove = null;
+    var computerMove = null;
+    while (playerWins < x && computerWins < x) {
+        playerMove=getPlayerMove();
+        computerMove=getComputerMove();
+        if (getWinner(playerMove, computerMove) == 'player') {
+    	   playerWins +=1
+        } else if (getWinner(playerMove, computerMove) == 'computer') {
+    	   computerWins +=1
+        }
+        console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+        console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+        if (playerWins == x) {
+            return 'Player Wins!'
+        } else if (computerWins == x) {
+            return 'Computer Wins!'
+        }
+    }
+}
